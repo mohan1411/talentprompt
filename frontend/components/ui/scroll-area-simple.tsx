77fcd863acb5c7aa6bdf4f2194b898894d6516de
mohan1411 <mohan.g1411@gmail.com>
2025-07-14 13:12:33 +0000

    fix: Add forwardRef support to ScrollArea component

--- frontend/components/ui/scroll-area-simple.tsx
+++ frontend/components/ui/scroll-area-simple.tsx
@@ -6,13 +6,18 @@ interface ScrollAreaProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
 }
 
-export function ScrollArea({ children, className = "", ...props }: ScrollAreaProps) {
-  return (
-    <div
-      className={`relative overflow-auto ${className}`}
-      {...props}
-    >
-      {children}
-    </div>
-  )
-}
+export const ScrollArea = React.forwardRef<HTMLDivElement, ScrollAreaProps>(
+  ({ children, className = "", ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={`relative overflow-auto ${className}`}
+        {...props}
+      >
+        {children}
+      </div>
+    )
+  }
+)
+
+ScrollArea.displayName = "ScrollArea"
